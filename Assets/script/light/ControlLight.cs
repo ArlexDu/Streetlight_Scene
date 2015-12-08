b3dfeda79bb4c1100aa8989a0b1e201c1eac8c21
Happyfine <772773671@qq.com>
2015-12-08 23:47:19 +0800

    加入kinect手势识别

--- Assets/script/light/ControlLight.cs
+++ Assets/script/light/ControlLight.cs
@@ -1,24 +1,40 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class ControlLight : MonoBehaviour {
 	private float last;
 	private bool move = false;
 	private int keep = 1;
 	private int stop = 0;
-    private bool turn = false;
-	// Use this for initialization
+	private LightGestureListener listener;
+	public bool slideChangeWithGestures = true;
+	public bool slideChangeWithKeys = true;
+ 	// Use this for initialization
 	void Start () {
-		
+		// hide mouse cursor
+		Screen.showCursor = false;
+		listener = GameObject.Find("Lights").GetComponent<LightGestureListener>();
 	}
 	
 	// Update is called once per frame
 	
 	void FixedUpdate(){
-		if (Input.GetKeyDown (KeyCode.C)) {
-			//添加力矩旋转
-			transform.GetComponent<Rigidbody> ().AddTorque (new Vector3 (0, 180, 0));
-            turn = false;
+		//键盘输入
+		if (slideChangeWithKeys) {
+			if (Input.GetKeyDown (KeyCode.C)) {
+				//添加力矩旋转
+				transform.GetComponent<Rigidbody> ().AddTorque (new Vector3 (0, 180, 0));
+			}
+		}
+		//手势输入
+		if (slideChangeWithGestures) {
+			KinectManager kinectManager = KinectManager.Instance;
+			if((!kinectManager || !kinectManager.IsInitialized() || !kinectManager.IsUserDetected()))
+				return;
+			if(listener.IsSwipeLeft()){
+				transform.GetComponent<Rigidbody> ().AddTorque (new Vector3 (0, 180, 0));
+		  }
 		}
 		if (Input.GetKeyDown (KeyCode.V)) {
 			if(move){
@@ -52,8 +68,4 @@ public class ControlLight : MonoBehaviour {
 		}
 	}
     
-    public void changturn()
-    {
-        turn = true;
-    }	
 }
